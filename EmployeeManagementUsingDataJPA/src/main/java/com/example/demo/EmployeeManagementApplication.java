c722752f3cb01438e5c71e6d3b2d72e9e61688ba
shakil <ahamadshakil2000@gmail.com>
2026-03-13 14:40:41 +0530

    second commit from sts

--- EmployeeManagementUsingDataJPA/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ EmployeeManagementUsingDataJPA/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
-
-	public static void main(String[] args) {
 	
+	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	
 	}
